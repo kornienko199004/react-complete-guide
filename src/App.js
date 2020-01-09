d352f3e755727a3a3571ce32a5b24ea3f11b0d0c
kornienko199004 <30842914+kornienko199004@users.noreply.github.com>
2020-01-09 16:51:39 +0300

    Add person component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -8,6 +9,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm React App</h1>
         <p>This is really working!</p>
+        <Person name="Alesey" age="29" />
+        <Person name="Manu" age="27">My hobbies: Racing</Person>
+        <Person name="Max" age="28" />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, I\'m React App'));
