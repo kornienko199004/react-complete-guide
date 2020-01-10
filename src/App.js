40ff9fac0808bf76e43b33979fa7349657199031
kornienko199004 <30842914+kornienko199004@users.noreply.github.com>
2020-01-10 10:10:24 +0300

    Add two-way data binding

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Aleksey', age: 29 },
+      { name: 'Manu', age: 27 },
+      { name: 'Max', age: 28 },
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 29 },
+        { name: 'Manu', age: 27 },
+        { name: 'Max', age: 20 },
+      ]
+    });
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Aleksey', age: 29 },
+        { name: event.target.value, age: 27 },
+        { name: 'Max', age: 20 },
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
         <p>This is really working!</p>
-        <Person name="Alesey" age="29" />
-        <Person name="Manu" age="27">My hobbies: Racing</Person>
-        <Person name="Max" age="28" />
+        <button onClick={() => this.switchNameHandler('Koko')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Koko')}
+          changed={this.nameChangedHandler}>My hobbies: Racing</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, I\'m React App'));
