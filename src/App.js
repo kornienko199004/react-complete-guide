f4115be7fb9e215b9da5955f5b15ef3134dc35fa
kornienko199004 <30842914+kornienko199004@users.noreply.github.com>
2020-01-14 09:44:43 +0300

    Changed styling to modules

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
-
-const StyledButton = styled.button`
-    background-color: ${props => props.alt ? 'red' : 'green'};
-    color: white;
-    font: inherit;
-    border: 1px solid blue;
-    padding: 8px;
-    cursor: pointer;
-
-    &:hover {
-        background-color: ${props => props.alt ? 'pink' : 'lightgreen'};
-        color: black;
-    }
-`;
 
 class App extends Component {
     state = {
@@ -54,6 +39,7 @@ class App extends Component {
 
     render() {
         let persons = null;
+        let btnClass = [classes.Button];
 
         if (this.state.showPersons) {
             persons = (
@@ -69,23 +55,24 @@ class App extends Component {
                     })}
                 </div>
             );
+            btnClass.push(classes.Red);
         }
 
-        let classes = [];
+        let assignedClasses = [];
         if (this.state.persons.length <= 2) {
-          classes.push('red');
+            assignedClasses.push(classes.red);
         }
         if (this.state.persons.length <= 1) {
-          classes.push('bold');
+            assignedClasses.push(classes.bold);
         }
 
         return (
-            <div className="App">
+            <div className={classes.App}>
                 <h1>Hi, I'm React App</h1>
-                <p className={classes.join(' ')}>This is really working!</p>
-                <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
+                <p className={assignedClasses.join(' ')}>This is really working!</p>
+                <button className={btnClass.join(' ')} onClick={this.togglePersonsHandler}>
                     Toggle persons
-                </StyledButton>
+                </button>
                 {persons}
             </div>
         );
